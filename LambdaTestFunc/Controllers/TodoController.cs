367dd679b703e13f4edf13a5caaf33b172f91c35
shshekeladze <shshekeladze@SSHEKELADZE>
2018-02-20 18:31:55 +0400

    Initial commit

---
+++ LambdaTestFunc/Controllers/TodoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LambdaTestFunc.Controllers
+{
+    [Route("api/Todo")]
+    public class TodoController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            
+            return new string[] { "buy milk", "walk dog", "call mom", "wash car", "buy medicine", "wash hands" };
+        }
+
+    }
+}
