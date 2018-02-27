2d1916fdd7b4e8dd7d1e3dbdf073490a1c5d9b69
shshekeladze <shshekeladze@SSHEKELADZE>
2018-02-27 10:43:17 +0400

    z75

--- LambdaTestFunc/Startup.cs
+++ LambdaTestFunc/Startup.cs
@@ -28,6 +28,7 @@ namespace LambdaTestFunc
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UsePathBase("/todo");
             app.UseMvc();
         }
     }
