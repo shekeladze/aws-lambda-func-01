edc2172aa8ffebe76de09991adc629a1d94b44b6
shshekeladze <shshekeladze@SSHEKELADZE>
2018-02-28 17:53:59 +0400

    added EnvTest

--- LambdaTestFunc/Controllers/TodoController.cs
+++ LambdaTestFunc/Controllers/TodoController.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace LambdaTestFunc.Controllers
 {
     [Route("api/Todo")]
     public class TodoController : Controller
     {
+        private IMemoryCache _cache;
+
+        public TodoController(IMemoryCache memCache)
+        {
+            _cache = memCache;
+        }
+
+
         [HttpGet]
         public IEnumerable<string> Get()
         {   
@@ -21,6 +30,33 @@ namespace LambdaTestFunc.Controllers
         public IEnumerable<string> Post(string someData)
         {
             return new string[] { "posted response" };
+        }
+
+        [HttpGet("EnvTest")]
+        public IActionResult EnvTest()
+        {
+            const string CACHED_DATA = "CACHED_DATA";
+
+            var envVar = Environment.GetEnvironmentVariable("SOME_VAR");
+            
+            string cachedStr;
+            var readFromCache = _cache.TryGetValue(CACHED_DATA, out cachedStr);
+
+            if (!readFromCache)
+            {
+                // Key not in cache, so get data.
+                cachedStr = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+
+                //// Set cache options.
+                //var cacheEntryOptions = new MemoryCacheEntryOptions()
+                //    // Keep in cache for this time, reset time if accessed.
+                //    .SetSlidingExpiration(TimeSpan.FromSeconds(3));
+
+                // Save data in cache.
+                _cache.Set(CACHED_DATA, cachedStr, DateTime.Now.AddMinutes(30));
+            }
+
+            return Ok(new { readFromCache, cachedValue = cachedStr });
         }
 
     }
